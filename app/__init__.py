dfcaacfb397ba087b36af0217de98f996b2a159f
Josh Haines <Josh@JoshHaines.com>
2016-02-10 15:23:16 -0500

    trying to add bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 """Inititalize the flask app."""
 
 from flask import Flask
-
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__, static_url_path="")
+Bootstrap(app)
 app.config.from_object('config')
 # pages = FlatPages(app)
 # db = SQLAlchemy(app)
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
-var gulp       = require('gulp');  
-var less       = require('gulp-less');
+// include gulp
+var gulp       = require('gulp');
 
-gulp.task('default', function() {  
+// include plugins
+var less       = require('gulp-less');
+var watch      = require('gulp-watch');
+
+// Task to compile less
+gulp.task('compile-less', function() {  
   gulp.src('./app/static/less/custom.less')
     .pipe(less())
     .pipe(gulp.dest('./app/static/css/'));
 });
+
+// Task to watch less changes
+gulp.task('watch-less', function() {
+	gulp.watch('./app/static/less/custom.less', ['compile-less']);
+});
+
+// Task when running 'gulp' from terminal
+gulp.task('default', ['compile-less', 'watch-less']);
